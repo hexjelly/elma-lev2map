d34fb8a3e59d757262affb65667912291a37b40f
Roger Andersen <hexjelly@hexjelly.com>
2017-03-15 00:27:40 +0100

    fix: move cmd line argument parsing to separate file
    fixes #2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 extern crate elma;
+#[macro_use]
 extern crate clap;
 
 use elma::lev::{ObjectType, Level};
 use elma::OBJECT_RADIUS;
-use clap::{Arg, App};
+use clap::App;
 
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
-
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 #[derive(Debug)]
 struct Settings<'a> {
@@ -19,133 +18,16 @@ struct Settings<'a> {
     flower: &'a str,
     killer: &'a str,
     player: &'a str,
-    stroke: usize,
-    scale: usize,
-    pad: usize,
-    width: usize,
-    height: usize,
-    max_width: usize,
-    max_height: usize,
+    stroke: u32,
+    scale: u32,
+    pad: u32,
     complexity: &'a str
 }
 
 fn main () {
     // Take care of command line arguments.
-    // TODO: put into external yaml file
-    let matches = App::new("elma-lev2map")
-                            .version(VERSION)
-                            .author("Roger Andersen <hexjelly@hexjelly.com>")
-                            .about("Converts Elasto Mania level files to images")
-                            .arg(Arg::with_name("input")
-                                .short("i").long("input")
-                                .value_name("PATH")
-                                .help("Path to level file")
-                                .takes_value(true).use_delimiter(false).required(true))
-                            .arg(Arg::with_name("output")
-                                .short("o").long("output")
-                                .value_name("PATH")
-                                .help("Path to save image file [default: <input>.svg]")
-                                .use_delimiter(false).takes_value(true))
-                            .arg(Arg::with_name("svg")
-                                .long("svg")
-                                .help("Specify SVG as output type [default]"))
-                            .arg(Arg::with_name("ground")
-                                .short("g").long("ground")
-                                .value_name("COLOR")
-                                .help("Ground fill color, in rgb, hex or name")
-                                .use_delimiter(false).takes_value(true)
-                                .default_value("#181048"))
-                            .arg(Arg::with_name("sky")
-                                .short("s")
-                                .long("sky")
-                                .value_name("COLOR")
-                                .help("Sky fill color, in rgb, hex or name")
-                                .use_delimiter(false)
-                                .default_value("#3078bc")
-                                .takes_value(true))
-                            .arg(Arg::with_name("pad")
-                                .long("pad")
-                                .value_name("SIZE")
-                                .help("Canvas padding")
-                                .default_value("10")
-                                .use_delimiter(false)
-                                .takes_value(true))
-                            .arg(Arg::with_name("scale")
-                                .long("scale")
-                                .value_name("SIZE")
-                                .help("Scale of SVG")
-                                .default_value("20")
-                                .use_delimiter(false)
-                                .takes_value(true))
-                            .arg(Arg::with_name("apple")
-                                .short("a")
-                                .long("apple")
-                                .value_name("COLOR")
-                                .help("Apple color, in rgb, hex or name")
-                                .use_delimiter(false)
-                                .default_value("red")
-                                .takes_value(true))
-                            .arg(Arg::with_name("flower")
-                                .short("f")
-                                .long("flower")
-                                .value_name("COLOR")
-                                .help("Flower color, in rgb, hex or name")
-                                .use_delimiter(false)
-                                .default_value("white")
-                                .takes_value(true))
-                            .arg(Arg::with_name("killer")
-                                .short("k")
-                                .long("killer")
-                                .value_name("COLOR")
-                                .help("Killer color, in rgb, hex or name")
-                                .use_delimiter(false)
-                                .default_value("black")
-                                .takes_value(true))
-                            .arg(Arg::with_name("player")
-                                .short("p")
-                                .long("player")
-                                .value_name("COLOR")
-                                .help("Player color, in rgb, hex or name")
-                                .use_delimiter(false)
-                                .default_value("green")
-                                .takes_value(true))
-                            .arg(Arg::with_name("stroke")
-                                .long("stroke")
-                                .value_name("SIZE")
-                                .help("Line stroke around objects")
-                                .use_delimiter(false)
-                                .default_value("0")
-                                .takes_value(true))
-                            .arg(Arg::with_name("height")
-                                .long("height")
-                                .value_name("SIZE")
-                                .help("Height")
-                                .conflicts_with("max_height")
-                                .use_delimiter(false).takes_value(true))
-                            .arg(Arg::with_name("width")
-                                .long("width")
-                                .value_name("SIZE")
-                                .help("Width")
-                                .conflicts_with("max_width")
-                                .use_delimiter(false).takes_value(true))
-                            .arg(Arg::with_name("max_height")
-                                .long("maxheight")
-                                .value_name("SIZE")
-                                .help("Max height")
-                                .use_delimiter(false).takes_value(true))
-                            .arg(Arg::with_name("max_width")
-                                .long("maxwidth")
-                                .value_name("SIZE")
-                                .help("Max width")
-                                .use_delimiter(false).takes_value(true))
-                            .arg(Arg::with_name("complexity")
-                                .long("complexity")
-                                .value_name("CHOICE")
-                                .help("What complexity to use for objects")
-                                .use_delimiter(false).takes_value(true)
-                                .default_value("complex")
-                                .possible_values(&["simple", "complex", "mix"]))
-                            .get_matches();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
     let input_file = Path::new(matches.value_of("input").unwrap());
     let mut output_file;
@@ -168,27 +50,10 @@ fn main () {
     let flower = matches.value_of("flower").unwrap();
     let killer = matches.value_of("killer").unwrap();
     let player = matches.value_of("player").unwrap();
-    let stroke = matches.value_of("stroke").unwrap().parse::<usize>().unwrap();
-    let scale = matches.value_of("scale").unwrap().parse::<usize>().unwrap();
-    let pad = matches.value_of("pad").unwrap().parse::<usize>().unwrap();
-    let mut width = 0;
-    // TODO: use clap default thing doh
-    if let Some(val) = matches.value_of("width") {
-        width = val.parse::<usize>().unwrap();
-    };
-    let mut max_width = 0;
-    if let Some(val) = matches.value_of("max_width") {
-        max_width = val.parse::<usize>().unwrap();
-    };
-    let mut height = 0;
-    if let Some(val) = matches.value_of("height") {
-        height = val.parse::<usize>().unwrap();
-    };
-    let mut max_height = 0;
-    if let Some(val) = matches.value_of("max_height") {
-        max_height = val.parse::<usize>().unwrap();
-    };
-    let complexity = matches.value_of("complexity").unwrap();
+    let stroke = matches.value_of("stroke").unwrap().parse::<u32>().unwrap();
+    let scale = matches.value_of("scale").unwrap().parse::<u32>().unwrap();
+    let pad = matches.value_of("pad").unwrap().parse::<u32>().unwrap();
+    let complexity = matches.value_of("svg").unwrap();
 
     let settings = Settings {
         ground: ground,
@@ -200,10 +65,6 @@ fn main () {
         stroke: stroke,
         scale: scale,
         pad: pad,
-        width: width,
-        height: height,
-        max_width: max_width,
-        max_height: max_height,
         complexity: complexity
     };
 
@@ -304,7 +165,7 @@ fn make_svg (input: &Path, settings: Settings, output: &PathBuf) {
         let y = ((object.position.y + min_y.abs()) * settings.scale as f64) + settings.pad as f64;
 
         // Simple circles.
-        if settings.complexity == "mix" || settings.complexity == "simple" {
+        if settings.complexity == "svg-mix" || settings.complexity == "svg-lo" {
             let color = match object.object_type {
                 ObjectType::Apple { .. } => settings.apple,
                 ObjectType::Exit => settings.flower,
@@ -317,7 +178,7 @@ fn make_svg (input: &Path, settings: Settings, output: &PathBuf) {
         }
 
         // SVG objects
-        if settings.complexity == "mix" || settings.complexity == "complex" {
+        if settings.complexity == "svg-mix" || settings.complexity == "svg-hi" {
             match object.object_type {
                 ObjectType::Apple { .. } => buffer.extend_from_slice(format!("<use x=\"{}\" y=\"{}\" xlink:href=\"#apple\" />\r\n", x - OBJECT_RADIUS * settings.scale as f64, y - OBJECT_RADIUS * settings.scale as f64).as_bytes()),
                 ObjectType::Killer => buffer.extend_from_slice(format!("<use x=\"{}\" y=\"{}\" xlink:href=\"#killer\" />\r\n", x - OBJECT_RADIUS * settings.scale as f64, y - OBJECT_RADIUS * settings.scale as f64).as_bytes()),
